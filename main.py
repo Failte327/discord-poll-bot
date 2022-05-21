52dc06818f7d8ee58350766d6811262ee90452e0
Timothy Twelker <timothy.twelker@gmail.com>
2022-05-21 11:24:00 -0600

    got the message detection + db changes fixed, working on embeds now

--- main.py
+++ main.py
@@ -4,27 +4,46 @@ from config import db
 from config import Poll
 from config import PollData
 from config import atlaspoll
+import asyncio
 import discord
+from discord.ext import commands
+import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\sqlite\\library.db'
 
 client = discord.Client()
-@client.event
-async def vote(message):
-    if message.content.startswith('!Albion'):
-        albionVote = PollData(option='Albion', poll=atlaspoll, user_id=message.author.id)
-        await db.session.add(albionVote)
-        db.session.commit()
-    elif message.content.startswith('!Hibernia'):
-        hiberniaVote = PollData(option='Hibernia', poll=atlaspoll, user_id=message.author.id)
-        await db.session.add(hiberniaVote)
-        db.session.commit()
-    elif message.content.startswith('!Midgard'):
-        midgardVote = PollData(option='Midgard', poll=atlaspoll, user_id=message.author.id)
-        await db.session.add(hiberniaVote)
-        db.session.commit()
 
-print(PollData.query.all())
+emb = discord.Embed(title='Atlas Launch Numbers')
+emb.add_field(name = 'Albion', value='', inline=False)
+emb.add_field(name = 'Hibernia', value='\n', inline=False)
+emb.add_field(name = 'Midgard', value='\n', inline=False)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!Albion'):
+        print ('Albion Vote Received')
+        albionVote = PollData(option='Albion', poll=atlaspoll, user_id=message.author.id)
+        db.session.add(albionVote)
+        db.session.commit()
+        channel = message.channel
+        print(PollData.query.all())
+        await channel.send('Vote Received!')
+    elif message.content.startswith('!Hibernia'):
+        print ('Hibernia Vote Received')
+        hiberniaVote = PollData(option='Hibernia', poll=atlaspoll, user_id=message.author.id)
+        db.session.add(hiberniaVote)
+        db.session.commit()
+        channel = message.channel
+        print(PollData.query.all())
+        await channel.send('Vote Received!')
+    elif message.content.startswith('!Midgard'):
+        print ('Midgard Vote Received')
+        midgardVote = PollData(option='Midgard', poll=atlaspoll, user_id=message.author.id)
+        db.session.add(midgardVote)
+        db.session.commit()
+        channel = message.channel
+        print(PollData.query.all())
+        await channel.send('Vote Received!')
 
 client.run('OTc3NDI2NDUwOTQyNjgxMTM4.GQ630h.mCVm7Nzn9Z8L-6yqEx0fm5dB_YnniXzKnKE3D8')
