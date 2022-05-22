f781671c45e3d1e1fe0012d835301108dfa9bb65
Timothy Twelker <timothy.twelker@gmail.com>
2022-05-22 07:29:25 -0600

    Embed working, count working, needs user ID check

--- main.py
+++ main.py
@@ -12,40 +12,55 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\sqlite\\library.db'
 
 client = discord.Client()
 
-emb = discord.Embed(title='Atlas Launch Numbers')
-emb.add_field(name = 'Albion', value='', inline=False)
-emb.add_field(name = 'Hibernia', value='\n', inline=False)
-emb.add_field(name = 'Midgard', value='\n', inline=False)
-
 @client.event
 async def on_message(message):
     if message.content.startswith('!Albion'):
         print ('Albion Vote Received')
         userid = message.author.id
-        print(PollData.query.filter_by(option='Albion').count())
         albionVote = PollData(option='Albion', poll=atlaspoll, user_id=userid)
         db.session.add(albionVote)
         db.session.commit()
         channel = message.channel
         print(PollData.query.all())
-        # This will query the count, we will need this. We just need a way to prohibit by 
-        # print(PollData.query.filter_by(option='Albion').count())
-        await channel.send('Vote Received!')
+        voteCountAlb = PollData.query.filter_by(option='Albion').count()
+        voteCountHib = PollData.query.filter_by(option='Hibernia').count()
+        voteCountMid = PollData.query.filter_by(option='Midgard').count()
+        chart = discord.Embed(title='Atlas Launch Numbers')
+        chart.add_field(name = 'Albion', value=voteCountAlb, inline=False)
+        chart.add_field(name = 'Hibernia', value=voteCountHib, inline=False)
+        chart.add_field(name = 'Midgard', value=voteCountMid, inline=False)
+        await channel.send(embed = chart)
     elif message.content.startswith('!Hibernia'):
         print ('Hibernia Vote Received')
-        hiberniaVote = PollData(option='Hibernia', poll=atlaspoll, user_id=message.author.id)
+        userid = message.author.id
+        hiberniaVote = PollData(option='Hibernia', poll=atlaspoll, user_id=userid)
         db.session.add(hiberniaVote)
         db.session.commit()
         channel = message.channel
         print(PollData.query.all())
-        await channel.send('Vote Received!')
+        voteCountAlb = PollData.query.filter_by(option='Albion').count()
+        voteCountHib = PollData.query.filter_by(option='Hibernia').count()
+        voteCountMid = PollData.query.filter_by(option='Midgard').count()
+        chart = discord.Embed(title='Atlas Launch Numbers')
+        chart.add_field(name = 'Albion', value=voteCountAlb, inline=False)
+        chart.add_field(name = 'Hibernia', value=voteCountHib, inline=False)
+        chart.add_field(name = 'Midgard', value=voteCountMid, inline=False)
+        await channel.send(embed = chart)
     elif message.content.startswith('!Midgard'):
         print ('Midgard Vote Received')
-        midgardVote = PollData(option='Midgard', poll=atlaspoll, user_id=message.author.id)
+        userid = message.author.id
+        midgardVote = PollData(option='Midgard', poll=atlaspoll, user_id=userid)
         db.session.add(midgardVote)
         db.session.commit()
         channel = message.channel
         print(PollData.query.all())
-        await channel.send('Vote Received!')
+        voteCountAlb = PollData.query.filter_by(option='Albion').count()
+        voteCountHib = PollData.query.filter_by(option='Hibernia').count()
+        voteCountMid = PollData.query.filter_by(option='Midgard').count()
+        chart = discord.Embed(title='Atlas Launch Numbers')
+        chart.add_field(name = 'Albion', value=voteCountAlb, inline=False)
+        chart.add_field(name = 'Hibernia', value=voteCountHib, inline=False)
+        chart.add_field(name = 'Midgard', value=voteCountMid, inline=False)
+        await channel.send(embed = chart)
 
 client.run('OTc3NDI2NDUwOTQyNjgxMTM4.GQ630h.mCVm7Nzn9Z8L-6yqEx0fm5dB_YnniXzKnKE3D8')
